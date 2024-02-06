ebc0322300b3f00d6a19d638d8ed1b5ac8206845
Yuriy <voytas1991@gmail.com>
2024-02-06 22:37:19 +0200

    added first endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
